b6c9685699c4ede049d0ddc0acbd1a8cb8ae07f6
Yasitha Bandara <yasithab@dmsswt.com>
2024-05-11 08:47:14 -0700

    mui installed and working.

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,6 +1,13 @@
 import Sheet from '../components/sheet/sheet';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 const Blogs = () => {
     return <Sheet>
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
     <h1>Purpose</h1>
     </Sheet>;
   };
